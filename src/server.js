d387bdc5def742d19d0591eaf7b23474e70ab97e
Simon Lopes <simonlopes99@gmail.com>
2023-05-31 01:05:57 -0300

    Config port

--- src/server.js
+++ src/server.js
@@ -2,4 +2,4 @@ const { serverHttp } = require("./http.js")
 
 require("./websocket.js")
 
-serverHttp.listen(3001, () => console.log("Server is runing"))
+serverHttp.listen(process.env.PORT || 3001, () => console.log("Server is runing"))
